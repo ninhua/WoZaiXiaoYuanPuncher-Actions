ca708303a2941819d9b01cd839b82e085cbcd455
ninhua <87905129+ninhua@users.noreply.github.com>
2021-10-17 11:34:47 +0800

    Update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import requests
+import json
+import os
+import utils
+from urllib.parse import urlencode
 print（os.environ['WZXY_LATITUDE']+os.environ['WZXY_LONGITUDE']+os.environ['WZXY_COUNTRY']+ os.environ['WZXY_CITY']+os.environ['WZXY_DISTRICT']+os.environ['WZXY_PROVINCE']+ os.environ['WZXY_TOWNSHIP']+os.environ['WZXY_STREET']）
