0677c6036df6320c5a697715605963f066d2488d
ninhua <87905129+ninhua@users.noreply.github.com>
2021-10-17 11:34:15 +0800

    Update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -1,1 +1,3 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 print（os.environ['WZXY_LATITUDE']+os.environ['WZXY_LONGITUDE']+os.environ['WZXY_COUNTRY']+ os.environ['WZXY_CITY']+os.environ['WZXY_DISTRICT']+os.environ['WZXY_PROVINCE']+ os.environ['WZXY_TOWNSHIP']+os.environ['WZXY_STREET']）
