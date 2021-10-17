c6d45e4ccdbce8e072ae38eb41dd1aaa8e28f27c
ninhua <87905129+ninhua@users.noreply.github.com>
2021-10-17 11:37:35 +0800

    Update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -5,4 +5,4 @@ import json
 import os
 import utils
 from urllib.parse import urlencode
-print（os.environ['WZXY_LATITUDE']+os.environ['WZXY_LONGITUDE']+os.environ['WZXY_COUNTRY']+ os.environ['WZXY_CITY']+os.environ['WZXY_DISTRICT']+os.environ['WZXY_PROVINCE']+ os.environ['WZXY_TOWNSHIP']+os.environ['WZXY_STREET']）
+print（os.environ['WZXY_USERNAME']）
