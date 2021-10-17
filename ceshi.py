5e1f43487f6a6ef9bbcaa429a883bda5919ba69f
ninhua <87905129+ninhua@users.noreply.github.com>
2021-10-17 11:30:37 +0800

    Create ceshi.py

---
+++ ceshi.py
@@ -0,0 +1,1 @@
+print（os.environ['WZXY_LATITUDE']+os.environ['WZXY_LONGITUDE']+os.environ['WZXY_COUNTRY']+ os.environ['WZXY_CITY']+os.environ['WZXY_DISTRICT']+os.environ['WZXY_PROVINCE']+ os.environ['WZXY_TOWNSHIP']+os.environ['WZXY_STREET'],）
